8143d5139c4c8e0387dff0f3b6369cd46a131db4
jiabo.zhou <jiabo.zhou@yingzt.com>
2018-02-01 22:13:42 +0800

    添加fork子进程

--- src/frame.c
+++ src/frame.c
@@ -1,15 +1,32 @@
+#include <unistd.h>
 
+int process;
 
-
-
-
-
-
-
-
-
-
-int main()
+static void work_process_cycle(void* )
 {
-	
+	for(;;)
+	{
+		printf("work process %d", process++);
+		sleep(10000);
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	for (int i = 0; i < 2; ++i)
+	{
+		pid_t pid = fork();
+		switch(pid)
+		{
+			case 0:
+				work_process_cycle(NULL);
+				break;
+			default:
+				break;
+		}
+
+	}
+	printf("master process");
+	return 0;
 }
