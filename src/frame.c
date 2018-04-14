748431490c8842e4673b6d70301cce283ed890a3
jiabo.zhou <jiabo.zhou@yingzt.com>
2018-04-14 22:52:48 +0800

    ok

--- src/frame.c
+++ src/frame.c
@@ -1,22 +1,31 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int process = 1;
+#define CPU_NUM 1
 
-static void work_process_cycle()
+int32_t process = 1;
+
+static int32_t work_process_cycle()
 {
 	printf("work process %d\n", process);
 	//todo 子进程搞事情
 }
 
-static void master_process_cycle()
+static int32_t master_process_cycle()
 {
 	//todo master搞事情
+	
 }
 
-int main(int argc, char* argv[])
+static int32_t Init()
 {
-	for (int i = 0; i < 2; ++i)
+	
+}
+
+int32_t main(int32_t argc, char* argv[])
+{
+
+	for (int32_t i = 0; i < CPU_NUM; ++i)
 	{
 		pid_t pid = fork();
 		switch(pid)
