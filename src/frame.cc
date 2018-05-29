33a6582bf08c79911fcb8b67bc75d2b3258b30ba
jiabo.zhou <jiabo.zhou@yingzt.com>
2018-05-29 13:32:28 +0800

    添加定时器，I/O事件处理

--- src/frame.cc
+++ src/frame.cc
@@ -7,6 +7,8 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <endian.h>
+#include <fcntl.h>
+#include <vector>
 
 #include "co_thread.h"
 
@@ -25,10 +27,10 @@ int32_t process;
 
 int set_fd_noblock(int fd)
 {
-	int flags = ::fcntl(fd, F_GETFL, 0);
+	int flags = fcntl(fd, F_GETFL, 0);
 	flags |= O_NONBLOCK;
 
-	int ret = ::fcntl(fd, F_SETFL, flags);
+	int ret = fcntl(fd, F_SETFL, flags);
 	if(0 != ret)
 	{
 
@@ -39,10 +41,10 @@ int set_fd_noblock(int fd)
 
 int set_fd_cloexec(int fd)
 {
-	int flags = ::fcntl(fd, F_GETFD, 0);
+	int flags = fcntl(fd, F_GETFD, 0);
 	flags |= FD_CLOEXEC;
 
-	int ret = ::fcntl(fd, F_SETFD, flags);
+	int ret = fcntl(fd, F_SETFD, flags);
 	if(0 != ret)
 	{
 
@@ -62,9 +64,65 @@ int set_fd_noblock_and_cloexec(int fd)
 	return -1;
 }
 
+typedef int (*event_handler)(connection_t *);
+
+typedef struct event_s
+{
+	event_handler handler;
+	void* arg;
+	char buf[1024];
+} event_t;
+
+typedef struct connection_s
+{
+	int fd;
+	event_t rev;
+	event_t wev;
+} connection_t;
+
+typedef struct task_s
+{
+	void* handler;
+	void* arg;
+} task_t;
+
+
+int read_handler(connection_t* c)
+{
+	event_t* p_rev = &c->rev;
+	int fd = c->fd;
+	char* buf = p_rev->buf;
+	size_t size_buf = 1024;
+	int ret = 0;
+	while((ret = read(fd, buf, size_buf)) != EAGAIN) 
+	{
+		printf("read buf:[%s]\n", buf);
+	}
+
+}
+
+int write_handler(connection_t* c)
+{
+	event_t* p_wev = &c->wev;
+	int fd = c->fd;
+	char* buf = p_wev->buf;
+	size_t size_buf = 1024;
+	int ret = 0;
+	while((ret = write(fd, buf, size_buf)) != EAGAIN) 
+	{
+		printf("write buf:[%s]\n", buf);
+	}
+}
+
+typedef timer_task_queue_t vector<task_t>;
+typedef io_task_queue_t vector<task_t>;
+
 static int32_t work_process_cycle()
 {
 	int ret = 0;
+	uint64_t timer = -1;
+	timer_task_queue_t timer_task_queue;
+	io_task_queue_t io_task_queue;
 	pid_t id = gettid();
 	printf("work process id:%d\n", id);
 	//todo 子进程搞事情
@@ -86,7 +144,8 @@ static int32_t work_process_cycle()
 		exit(ret);
 	}
 
-	struct epoll_event* ee = {
+	struct epoll_event* ee = 
+	{
 		EPOLLIN | EPOLLRDHUP,
 		lfd,
 	};
@@ -104,22 +163,23 @@ static int32_t work_process_cycle()
 		uint64_t idx = top;
 		if(now >= top)
 		{
-			for_each(ite it:timer_queue)
+			for_each(timer_queue.begin(), timer_queue.end(), [](timer_task_queue_t::iterator it) 
 			{
 				if(now >= *it)
 				{
 					timer_task_queue.push(*it);
 					idx++;
 				}
-			}
+			});
+
+			timer_queue.remove(top, idx);
 		}
-		timer_queue.remove(top, idx);
 
 		// -------------- 定时器事件 --------------
-		for_each(ite it:timer_task_queue)
+		for_each(timer_task_queue.begin(), timer_task_queue.end(), [](timer_task_queue_t::iterator it) 
 		{
-			it->handler();
-		}
+			it->handler(it->arg);
+		});
 
 		timer_task_queue.clear();
 
@@ -133,10 +193,10 @@ static int32_t work_process_cycle()
 		}
 
 		// -------------- 网络 I/O 的读写事件 --------------
-		for_each(ite it:io_task_queue)
+		for_each(io_task_queue.begin(), io_task_queue.end(), [](io_task_queue_t::iterator it) 
 		{
-			it->handler();
-		}
+			it->handler(it->arg);
+		});
 
 		io_task_queue.clear();
 
@@ -147,9 +207,10 @@ static int32_t work_process_cycle()
 
 		for (int i = 0; i < cnt; ++i)
 		{
-			struct epoll_event* ee = p_ee[i];
-			uint32_t events = ee->events;
-			int fd = ee->data.fd;
+			struct epoll_event* ee 	= p_ee[i];
+			uint32_t events 		= ee->events;
+			connection_t* c 		= (connection_t *)ee->data.ptr;
+			int fd = c->fd;
 
 			if(fd == lfd)
 			{
@@ -164,15 +225,24 @@ static int32_t work_process_cycle()
 
 				}
 
+				connection_t* p_conn = new connection_t();
+				p_conn->fd = fd;
+				p_conn->rev.handler = read_handler;
+				p_conn->rev.arg = p_conn;
+
+				p_conn->wev.handler = write_handler;
+				p_conn->wev.arg = p_conn;
+
 				struct epoll_event* ee = 
 				{
 					EPOLLIN | EPOLLRDHUP | EPOLLOUT,
-					fd,
+					p_conn,
 				};
+
 				ret = epoll_ctl(efd, EPOLL_CTL_ADD, fd, ee);
 				if(0 != ret)
 				{
-
+					
 				}
 			}
 			else
@@ -180,12 +250,18 @@ static int32_t work_process_cycle()
 				if(events | EPOLLIN | EPOLLRDHUP)
 				{
 					//加入读事件队列
+					task_t task;
+					task.handler = read_handler;
+					task.arg = c;
 					io_task_queue.push(task);
 				}
 				
 				if(events | EPOLLOUT)
 				{
 					//加入写事件队列
+					task_t task;
+					task.handler = write_handler;
+					task.arg = c;
 					io_task_queue.push(task);
 				}
 			}
